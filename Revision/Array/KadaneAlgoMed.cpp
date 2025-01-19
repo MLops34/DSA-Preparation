291d7fcee523443c3c86897ed4f38c89771cf168
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-01-19 17:02:12 +0530

    Update KadaneAlgoMed.cpp

--- Revision/Array/KadaneAlgoMed.cpp
+++ Revision/Array/KadaneAlgoMed.cpp
@@ -22,13 +22,23 @@ class Solution{
         }
         return maxi;
     }
+
+    int OptimalMSA(vector<int>&arr){
+        long long sum=0,maxi=INT_MIN;
+        for(int i=0;i<arr.size();i++){
+            sum=sum+arr[i];
+            maxi=max(maxi,sum);
+            if(sum<0) sum=0;
+        }
+        return maxi;
+    }
 };
 
 int main()
 {
     Solution sol;
     vector<int>arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
-    long long maxSum = sol.BetterMaxiSubArray(arr);
+    long long maxSum = sol.OptimalMSA(arr);
     cout << "The maximum subarray sum is: " << maxSum << endl;
     return 0;
 }
