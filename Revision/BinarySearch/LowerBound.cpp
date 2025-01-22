464af808a9cb593b4a6e2e8997cc6597cd9117a2
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-01-22 21:18:18 +0530

    Create LowerBound.cpp

---
+++ Revision/BinarySearch/LowerBound.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+    public:
+    int LowerBound(vector<int>Arr,int target){
+        int low=0,high=Arr.size()-1;
+        int ans;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(Arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return ans;
+    }
+
+};
+
+int main(){
+    Solution sol;
+    vector<int>Arr={2,4,6,8,9,10,12,14,16,18,20};
+    int Func=sol.LowerBound(Arr,7);
+    cout << "Answer-->" << Func;
+    return 0;
+}
