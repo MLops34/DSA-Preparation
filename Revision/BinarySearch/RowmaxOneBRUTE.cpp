80f888dbb6035bbab734c2a524522222b3fd9da3
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-01-27 13:56:37 +0530

    Create RowmaxOneBRUTE.cpp

---
+++ Revision/BinarySearch/RowmaxOneBRUTE.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// #include <limits>
+
+// using namespace std;
+// class solution{
+//     public:
+// //     int rowWithMax1(vector<vector<int>>& mat){
+// //         int n=mat[0].size(),m=mat.size();
+// //         int cnt=0 ,maxi=0,row=-1;
+// //         for(int i=0;i<n-1;i++){
+// //             cnt=0;
+// //             for(int j=0;j<m-1;j++){
+// // // 
+// //                     cnt=cnt+mat[i][j];
+// //             }
+// //             if(cnt>maxi){
+// //                 cnt=maxi;
+// //                 row=i;
+// //             }
+
+// //         }
+// //         return row;
+// //     }
+// // };
+
+// int main(){
+//     solution sol;
+//     vector<vector<int>>temp={{{0,1,1,1},{0,0,0,0},{1,1,1,1},{0,0,0,1},{0,1,1,1},{0,0,0,1},{0,0,0,1}}};
+//     int ans=sol.rowWithMax1(temp);
+//     cout << ans;
+//     return 0;
+// }
+
+
