76db02351a67bb3bc7a726c0cb9069ede9423e7e
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-01-31 19:20:48 +0530

    Update MaxOnes.cpp

--- Revision/Array/MaxOnes.cpp
+++ Revision/Array/MaxOnes.cpp
@@ -8,22 +8,23 @@ using namespace std;
 class Solution{
     public:
     int maxOnes(vector<int>&arr){
-        int cnt=1;
-        for(int i=0;i<arr.size();i++){
-            if(i>0 && arr[i-1]==arr[i]){
+        int cnt=1;int fun=0;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i-1]==arr[i]){
                 cnt++;
             }else{
                 cnt=1;
+                fun=arr[i-1];
             }
         }
-        return cnt;
+        return fun;
     }
 
 };
 
 int main(){
     Solution sol;
-    vector<int>arr={1,1,0,1,1,1};
+    vector<int>arr={4,1,2,1,2};
     int ans=sol.maxOnes(arr);
     cout<<ans;
     return 0;
