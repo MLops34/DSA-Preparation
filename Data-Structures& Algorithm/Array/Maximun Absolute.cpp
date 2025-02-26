7326d497982d6b270a236308938312246e23dc53
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-02-26 23:29:23 +0530

    Create Maximun Absolute.cpp

---
+++ Data-Structures& Algorithm/Array/Maximun Absolute.cpp
@@ -0,0 +1,20 @@
+int n = nums.size();
+
+int currSubSum = nums[0];
+int maxSubSum = nums[0];
+
+//Kadane's Algo for findig max subarray sum
+for(int i = 1; i < n; i++) {
+    currSubSum = max(nums[i], currSubSum + nums[i]);
+    maxSubSum = max(maxSubSum, currSubSum);
+}
+
+//Kadane's Algo for findig min subarray sum
+int minSubSum = nums[0];
+currSubSum = nums[0];
+for(int i = 1; i < n; i++) {
+    currSubSum = min(nums[i], currSubSum + nums[i]);
+    minSubSum = min(minSubSum, currSubSum);
+}
+
+return max(maxSubSum, abs(minSubSum));  s
