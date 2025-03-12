32cf1b84b477cd8aa7136e5e50122da8d0cc497b
MLops34 <147312087+MLops34@users.noreply.github.com>
2025-03-13 00:32:05 +0530

    Create Maximum Count of Positive Integer and Negative Integer.cpp

---
+++ Data-Structures& Algorithm/Sliding Window/Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,14 @@
+Maximum Count of Positive Integer and Negative Integer
+
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int c_p = 0,c_n =0;
+            for(int i =0 ;i<nums.size();i++)
+            {
+                if(nums[i] < 0) c_n++;
+                if(nums[i] > 0) c_p++;
+            }
+            return max(c_n,c_p);
+        }
+    };
